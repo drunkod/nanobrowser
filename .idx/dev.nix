5aec46e702811b868892301d55a0f1e4f0a83597
drunkod <drunkod@gmail.com>
2025-03-12 13:14:33 +0000

    add dev nix

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+{pkgs}: {
+  channel = "unstable";
+  packages = [
+    pkgs.nodejs_22
+    pkgs.pnpm
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
